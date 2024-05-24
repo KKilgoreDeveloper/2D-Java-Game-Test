9eeac456e0007f18ab3035b80981c5918f45bfa2
Kyndall Kilgore <kyndallf25@gmail.com>
2024-05-24 10:54:50 -0500

    Added JFrame window

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        //Lets user exit window by pressing 'x'
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Makes window unable to be resized
+        window.setResizable(false);
+        //Window title
+        window.setTitle("2D Game Test");
+        //centers window location to the middle of the screen
+        window.setLocationRelativeTo(null);
+    }
+}

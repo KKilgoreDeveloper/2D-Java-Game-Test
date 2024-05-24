3777cf38f1ed78426d78ef1757d414c60fc6808b
Kyndall Kilgore <kyndallf25@gmail.com>
2024-05-24 11:45:01 -0500

    Added screen settings

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,23 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 tile
+    //Scaling
+    final int scale = 3;
+    final int tileSize= originalTileSize * scale; //48x48tile
+    final int maxScreenCol =16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
 }

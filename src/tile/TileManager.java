754837aa3c39e194ae8ef481743550ec43dd2f91
Kyndall Kilgore <kyndallf25@gmail.com>
2024-05-24 23:02:13 -0500

    Added MORE tiles and tile map :)

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,16 +4,22 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum[] [];
 
     public TileManager(GamePanel gp){
         this.gp = gp;
-        tile = new Tile[10];
+        tile = new Tile[11];
+        mapTileNum = new int [gp.maxScreenCol] [gp.maxScreenRow];
         getTileImage();
+        loadMap();
     }
     public void getTileImage() {
         try {
@@ -23,15 +29,56 @@ public class TileManager {
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/adobewallupdated.png"));
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wateredge-down.png"));
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wateredge-up.png"));
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wateredge-left.png"));
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wateredge-right.png"));
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream("/tiles/watercorner-leftdown.png"));
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream("/tiles/watercorner-rightdown.png"));
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream("/tiles/watercorner-leftup.png"));
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream("/tiles/watercorner-rightup.png"));
         }catch (IOException e){
             e.printStackTrace();
         }
     }
-    public void draw(Graphics2D g2) {
 
-        //g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
-       // g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
-       // g2.drawImage(tile[2].image, 96, 0, gp.tileSize, gp.tileSize, null);
+    public void loadMap() {
+
+        try {
+            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+            while (col < gp.maxScreenCol && row < gp.maxScreenRow){
+
+                String line = br.readLine();
+                while(col < gp.maxScreenCol){
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt (numbers[col]);
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if (col == gp.maxScreenCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+
+        }
+    }
+    public void draw(Graphics2D g2) {
 
         int col = 0;
         int row = 0;
@@ -39,7 +86,9 @@ public class TileManager {
         int y = 0;
 
         while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
-            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
             col++;
             x += gp.tileSize;
 

9e6d7b2fee551096542c6d35a7448b6745236db2
Karlo Kilgore <kyndallf25@gmail.com>
2024-12-26 21:39:33 -0500

    Fawn moves (normally)

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -22,6 +22,7 @@ public class Entity {
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
     public int solidAreaDefaultX, solidAreaDefaultY;
     public boolean collisionOn = false;
+    public int actionLockCounter = 0;
 
     public Entity(GamePanel gp){
         this.gp = gp;
--- src/entity/NPC_Fawn.java
+++ src/entity/NPC_Fawn.java
@@ -28,21 +28,28 @@ public class NPC_Fawn extends Entity{
     }
     public void setAction(){
 
-        Random random = new Random();
-        int i = random.nextInt(100)+1; //pick number from 1 to 100
+        actionLockCounter++;
 
-        if(i <= 25) {
-            direction = "up";
+        if(actionLockCounter == 120) {
+
+            Random random = new Random();
+            int i = random.nextInt(100) + 1; //pick number from 1 to 100
+
+            if (i <= 25) {
+                direction = "up";
+            }
+            if (i > 25 && i <= 50) {
+                direction = "down";
+            }
+            if (i > 50 && i <= 75) {
+                direction = "left";
+            }
+            if (i > 75 && i <= 100) {
+                direction = "right";
+            }
+            actionLockCounter = 0;
         }
-        if(i > 25 && i <= 50) {
-            direction = "down";
-        }
-        if(i > 50 && i <= 75) {
-            direction = "left";
-        }
-        if(i > 75 && i <= 100) {
-            direction = "right";
-        }
+
     }
 
 }

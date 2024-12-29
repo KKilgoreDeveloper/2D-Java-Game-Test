c4b3fb628d9b648c8b4c07bddd88ba6a76967564
Karlo Kilgore <kyndallf25@gmail.com>
2024-12-29 16:11:55 -0500

    Title screen added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -43,6 +43,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     //GAME STATE
     public int gameState;
+    public final int titleState = 0;
     public final int playState = 1;
     public final int pauseState = 2;
     public final int dialogueState = 3;
@@ -62,7 +63,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         playMusic(0);
         stopMusic();
-        gameState = playState;
+        gameState = titleState;
     }
 
     public void startGameThread(){
@@ -125,27 +126,34 @@ public class GamePanel extends JPanel implements Runnable {
             drawStart = System.nanoTime();
         }
 
-        //TILE
-        tileM.draw(g2);
-
-        //OBJECT
-        for(int i =0; i < obj.length; i++){
-            if (obj[i] != null){
-                obj[i].draw(g2, this);
-            }
-        }
-        //NPC
-        for(int i =0; i < npc.length; i++){
-            if (npc[i] != null){
-                npc[i].draw(g2);
-            }
-        }
-
-        //PLAYER
-        player.draw(g2);
-
-        //UI
+        //TITLE SCREEN
+        if (gameState == titleState){
         ui.draw(g2);
+        }
+        //OTHERS
+        else{
+            //TILE
+            tileM.draw(g2);
+
+            //OBJECT
+            for(int i =0; i < obj.length; i++){
+                if (obj[i] != null){
+                    obj[i].draw(g2, this);
+                }
+            }
+            //NPC
+            for(int i =0; i < npc.length; i++){
+                if (npc[i] != null){
+                    npc[i].draw(g2);
+                }
+            }
+
+            //PLAYER
+            player.draw(g2);
+
+            //UI
+            ui.draw(g2);
+        }
 
         //DEBUG
         if (keyH.checkDrawTime == true){
--- src/main/UI.java
+++ src/main/UI.java
@@ -18,6 +18,7 @@ public class UI {
     int messageCounter = 0;
     public boolean gameFinished = false;
     public String currentDialogue = "";
+    public int commandNum = 0;
 
     public UI(GamePanel gp){
         this.gp = gp;
@@ -42,6 +43,11 @@ public class UI {
 
         g2.setFont(lowresPixel);
         g2.setColor(Color.white);
+
+        //TITLE STATE
+        if (gp.gameState == gp.titleState){
+            drawTitleScreen();
+        }
         //PLAY STATE
         if(gp.gameState == gp.playState){
             // Do playState stuff later
@@ -54,6 +60,55 @@ public class UI {
         if (gp.gameState == gp.dialogueState){
             drawDialogueScreen();
         }
+    }
+    public void drawTitleScreen(){
+        g2.setColor(new Color(128,63,42));
+        g2.fillRect(0,0,gp.screenWidth,gp.screenHeight);
+
+        // TITLE NAME
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD,96F));
+        String text = "Coyote Game";
+        int x = getXforCenteredText(text);
+        int y = gp.tileSize*3;
+
+        //SHADOW
+        g2.setColor(Color.darkGray);
+        g2.drawString(text,x+5,y+5);
+        //MAIN COLOR
+        g2.setColor(Color.white);
+        g2.drawString(text, x, y);
+
+        //COYOTE IMAGE
+        x = gp.screenWidth/2 - (gp.tileSize*2)/2;
+        y += gp.tileSize*2;
+        g2.drawImage(gp.player.down1, x, y, gp.tileSize*2, gp.tileSize*2, null);
+
+        // MENU
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD,48F));
+        text = "NEW GAME";
+        x = getXforCenteredText(text);
+        y += gp.tileSize*3.5;
+        g2.drawString(text, x, y);
+        if (commandNum == 0){
+            g2.drawString(">", x- gp.tileSize, y);
+        }
+
+        text = "LOAD GAME";
+        x = getXforCenteredText(text);
+        y += gp.tileSize;
+        g2.drawString(text, x, y);
+        if (commandNum == 1){
+            g2.drawString(">", x- gp.tileSize, y);
+        }
+
+        text = "QUIT";
+        x = getXforCenteredText(text);
+        y += gp.tileSize;
+        g2.drawString(text, x, y);
+        if (commandNum == 2){
+            g2.drawString(">", x- gp.tileSize, y);
+        }
+
     }
     public void drawPauseScreen (){
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));

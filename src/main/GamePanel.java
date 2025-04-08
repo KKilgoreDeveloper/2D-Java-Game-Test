e8862a421a513c85c883b48208e188aa84084b6a
Karlo Kilgore <kyndallf25@gmail.com>
2025-04-08 16:58:17 -0400

    added event handler class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -34,6 +34,7 @@ public class GamePanel extends JPanel implements Runnable {
     public CollisionCheck cCheck = new CollisionCheck(this);
     public AssetSetter aSetter = new AssetSetter(this);
     public UI ui = new UI(this);
+    public EventHandler eHandler = new EventHandler(this);
     Thread gameThread;
 
     //ENTITY AND OBJECT

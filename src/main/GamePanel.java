081a37bd1a7e514ee9f190ce3d671bdec441f37c
Karlo Kilgore <kyndallf25@gmail.com>
2025-04-08 20:23:31 -0400

    removed SuperObject class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -38,7 +38,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     //ENTITY AND OBJECT
     public Player player = new Player(this,keyH);
-    public SuperObject obj[] = new SuperObject[10];
+    public Entity obj[] = new Entity[10];
     public Entity npc[] = new Entity[10];
 
     //GAME STATE

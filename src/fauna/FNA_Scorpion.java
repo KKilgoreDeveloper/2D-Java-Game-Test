80703b9c6e5937bb8c12b1fde223244f4ef0a574
Karlo Kilgore <kyndallf25@gmail.com>
2025-06-25 12:01:06 -0400

    added right scorpion frames, renamed left frames, and created scorpion class

---
+++ src/fauna/FNA_Scorpion.java
@@ -0,0 +1,12 @@
+package fauna;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class FNA_Scorpion extends Entity{
+    public FNA_Scorpion(GamePanel gp) {
+        super(gp);
+        name = "Scorpion";
+        speed = 1;
+    }
+}
